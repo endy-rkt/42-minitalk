4921d1a9dfc4709e83e7a8ffa9efeb77053bb210
endy-rkt <zk818019@gmail.com>
2024-05-23 00:28:41 +0200

    :sparkles: add naive approach

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: trazanad <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 20:39:16 by trazanad          #+#    #+#             */
-/*   Updated: 2024/05/22 22:21:56 by trazanad         ###   ########.fr       */
+/*   Updated: 2024/05/23 00:25:05 by trazanad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,12 @@ void	send_character(pid_t server_pid, char character)
 		i--;
 		tmp = character >> i;
 		if (tmp % 2 == 0)
-			write(1, "0",1);//send 0
+			kill(server_pid, SIGUSR1);// write(1, "0",1);//send 0
 		else
-			write(1, "1",1);//send 1
-		usleep(42);//change value
+			kill(server_pid, SIGUSR2);// write(1, "1",1);//send 1
+		usleep(100);//change value
 	}
+		usleep(100);//change value
 }
 
 void	send_msg(pid_t server_pid, char *msg)
@@ -38,9 +39,8 @@ void	send_msg(pid_t server_pid, char *msg)
 	while (msg[i])
 	{
 		send_character(server_pid, msg[i]);
-		write(1, "\n",1);//
 		i++;
-		usleep(50);//can we take it
+		// usleep();//can we take it
 	}
 	send_character(server_pid, '\0');//msg end
 }
@@ -55,7 +55,7 @@ int main(int argc, char *argv[])
 	if (server_pid <= 0)
 		exit(EXIT_FAILURE);
 	// printf("Ready to send msg (%s) to %d",argv[2], server_pid);//
-	//send_msg
+	//send_msg 4386
 	send_msg(server_pid, argv[2]);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,42 +6,47 @@
 /*   By: trazanad <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 20:50:20 by trazanad          #+#    #+#             */
-/*   Updated: 2024/05/22 22:42:40 by trazanad         ###   ########.fr       */
+/*   Updated: 2024/05/23 00:25:20 by trazanad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-//binary to char
-void	print(char *str)
+void handle_sigusr(int signum, siginfo_t *info, void *context)
 {
-	int i,j;
-	unsigned char character,tmp;
+	static unsigned int i = 8;
+	static unsigned char character = 0;
+	unsigned char tmp;
 
-	i = 8;
-	j = 0;
-	character = 0;
 	tmp = 1;
-	while (i > 0)
+	if (i > 0)
 	{
 		i--;
-		if (str[j] == '1')
-			character |= tmp << i; //character = character | tmp << i;
-		j++;
+		if (signum == SIGUSR2)
+			character |= tmp << i; 
 	}
-	printf("%c %d\n",character,character);
+	if (i == 0)
+	{
+		write(1,&character,1);
+		usleep(42);//
+		i = 8;
+		character = 0;
+	}
 }
 
 int main(int argc, char *argv[])
 {
 	pid_t pid;
-
+	struct sigaction sa;
+	
 	pid = getpid();
 	printf("%d\n",pid);
+	sa.sa_sigaction = &handle_sigusr;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	//listen for client signal
 	//print message
 	//wait infinitly for signal
-	print(argv[1]);
-	// while(1);
+	while(1);
 	return (0);
 }
