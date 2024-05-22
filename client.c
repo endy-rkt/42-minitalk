0611ec6535fa83ac0af24e1ccca8de8b80c60509
endy-rkt <zk818019@gmail.com>
2024-05-22 22:44:56 +0200

    :sparkles: test logic for char sending and byte to char

--- client.c
+++ client.c
@@ -6,11 +6,44 @@
 /*   By: trazanad <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 20:39:16 by trazanad          #+#    #+#             */
-/*   Updated: 2024/05/22 20:54:19 by trazanad         ###   ########.fr       */
+/*   Updated: 2024/05/22 22:21:56 by trazanad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	send_character(pid_t server_pid, char character)
+{
+	int i;
+	unsigned char tmp;
+
+	i = 8;
+	while(i > 0)
+	{
+		i--;
+		tmp = character >> i;
+		if (tmp % 2 == 0)
+			write(1, "0",1);//send 0
+		else
+			write(1, "1",1);//send 1
+		usleep(42);//change value
+	}
+}
+
+void	send_msg(pid_t server_pid, char *msg)
+{
+	int i;
+
+	i = 0;
+	while (msg[i])
+	{
+		send_character(server_pid, msg[i]);
+		write(1, "\n",1);//
+		i++;
+		usleep(50);//can we take it
+	}
+	send_character(server_pid, '\0');//msg end
+}
 
 int main(int argc, char *argv[])
 {
@@ -21,6 +54,8 @@ int main(int argc, char *argv[])
 	server_pid = atoi(argv[1]);
 	if (server_pid <= 0)
 		exit(EXIT_FAILURE);
+	// printf("Ready to send msg (%s) to %d",argv[2], server_pid);//
 	//send_msg
+	send_msg(server_pid, argv[2]);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,13 +6,33 @@
 /*   By: trazanad <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 20:50:20 by trazanad          #+#    #+#             */
-/*   Updated: 2024/05/22 20:55:11 by trazanad         ###   ########.fr       */
+/*   Updated: 2024/05/22 22:42:40 by trazanad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int main(void)
+//binary to char
+void	print(char *str)
+{
+	int i,j;
+	unsigned char character,tmp;
+
+	i = 8;
+	j = 0;
+	character = 0;
+	tmp = 1;
+	while (i > 0)
+	{
+		i--;
+		if (str[j] == '1')
+			character |= tmp << i; //character = character | tmp << i;
+		j++;
+	}
+	printf("%c %d\n",character,character);
+}
+
+int main(int argc, char *argv[])
 {
 	pid_t pid;
 
@@ -21,6 +41,7 @@ int main(void)
 	//listen for client signal
 	//print message
 	//wait infinitly for signal
-	while(1);
+	print(argv[1]);
+	// while(1);
 	return (0);
 }
